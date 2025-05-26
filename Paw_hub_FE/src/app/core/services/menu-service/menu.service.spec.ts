c09b065982bafb12808a111b1572ef5928972653
Mezikt97 <nunomesquita97@gmail.com>
2025-05-26 03:29:36 +0100

    feat: implement category and menu services with CRUD operations, and add restaurant model

--- Paw_hub_FE/src/app/core/services/menu-service/menu.service.spec.ts
+++ Paw_hub_FE/src/app/core/services/menu-service/menu.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MenuService } from './menus.service';
+import { MenuService } from './menu.service';
 
 describe('MenusService', () => {
   let service: MenuService;
