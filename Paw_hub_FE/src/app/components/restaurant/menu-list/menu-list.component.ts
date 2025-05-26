81f2e7844d2982a724e1bb50572c48d6de91fe82
Mezikt97 <nunomesquita97@gmail.com>
2025-05-26 04:17:31 +0100

    feat: implement filters panel component with filtering functionality for menu items

--- Paw_hub_FE/src/app/components/restaurant/menu-list/menu-list.component.ts
+++ Paw_hub_FE/src/app/components/restaurant/menu-list/menu-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MenuService } from '../../../core/services/menu-service/menu.service';
 import { Menu } from '../../../models/menu.model';
 import { AuthService } from '../../../core/auth-service/auth.service';
+import { Category } from '../../../models/category.model';
+import { CategoryService } from '../../../core/services/category-service/category-service.service';
 
 @Component({
   selector: 'app-menu-list',
@@ -10,16 +12,24 @@ import { AuthService } from '../../../core/auth-service/auth.service';
 })
 export class MenuListComponent implements OnInit {
   menus: Menu[] = [];
+  categories: Category[] = [];
   loading = false;
   error?: string;
+  filters = {
+    name: '',
+    categoryId: '',
+    available: ''
+  };
 
   constructor(
     private menuService: MenuService,
+    private categoryService: CategoryService,
     private authService: AuthService
   ) {}
 
   ngOnInit(): void {
     this.loadMenus();
+    this.loadCategories();
   }
 
   loadMenus() {
@@ -34,6 +44,28 @@ export class MenuListComponent implements OnInit {
         this.error = 'Erro ao carregar menus';
         this.loading = false;
       }
+    });
+  }
+
+  loadCategories() {
+    const restaurantId = this.getRestaurantId();
+    this.categoryService.getCategoriesByRestaurant(restaurantId).subscribe({
+      next: (categories) => (this.categories = categories)
+    });
+  }
+
+  onFiltersChanged(filters: any) {
+    this.filters = filters;
+  }
+
+  filteredMenus(): Menu[] {
+    return this.menus.filter(menu => {
+      const nameMatch = !this.filters.name || menu.name.toLowerCase().includes(this.filters.name.toLowerCase());
+      const categoryMatch = !this.filters.categoryId || menu.categoryId === this.filters.categoryId;
+      const availableMatch =
+        this.filters.available === '' ||
+        menu.available === (this.filters.available === 'true');
+      return nameMatch && categoryMatch && availableMatch;
     });
   }
 
